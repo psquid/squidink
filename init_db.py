ffea2ca719621849378c0fe2cfe78f9faae7def3
Psychedelic Squid <psquid@psquid.net>
2011-06-22 00:55:10 +0100

    Fixed glaring errors in init script.

--- init_db.py
+++ init_db.py
@@ -6,18 +6,19 @@ import hashlib
 db = redis.Redis()
 
 while True:
-    initial_admin = raw_input("What username should your initial admin have?\n").strip().lower()
+    initial_admin = raw_input("What username should your initial admin have?\t").strip().lower()
     if initial_admin != "":
         break
 while True:
-    initial_admin_pass = raw_input("What should your initial admin's password be?\n").strip().lower()
+    initial_admin_pass = getpass.getpass("What should your initial admin's password be?\t").strip().lower()
     if initial_admin_pass != "":
         break
 while True:
-    sitename = raw_input("What should your site's title be?\n").strip().lower()
+    sitename = raw_input("What should your blog's title be?\t").strip()
     if sitename != "":
         break
 
-db.set("user:{0}:hashed_pw".format(initial_admin), hashlib.md5(hashlib.md5(initial_admin_pass).hex_digest()).hex_digest())
+db.set("users:{0}:hashed_pw".format(initial_admin), hashlib.md5(hashlib.md5(initial_admin_pass).hexdigest()).hexdigest())
 db.sadd("admins", initial_admin)
+db.sadd("users", initial_admin)
 db.set("sitename", sitename)
